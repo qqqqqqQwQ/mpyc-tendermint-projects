ae7aa83919be808340b47d84019ab901260c3d5e
qqqqqqQwQ <1062861883@qq.com>
2024-04-25 20:36:50 +0800

    1

--- host/tests/testModel.py
+++ host/tests/testModel.py
@@ -15,5 +15,19 @@ decision_tree = text2treeArray.parse_tree(tree_text)
 data = pd.read_csv(data_path)
 dataset = dataClean.process_excel_file(data)
 # 使用 validate_dataset 函数对数据集进行验证
+loan_status_result = dataset['Loan_Status'].values
+print(loan_status_result)
 predictions = dataAnalyseBytxt.validate_dataset(dataset, decision_tree)
 print(predictions)
+
+# 初始化准确度计数器
+accuracy_count = 0
+
+# 比较两个数组对应位置的值
+for i in range(len(loan_status_result)):
+    if loan_status_result[i] == predictions[i]:
+        accuracy_count += 1
+
+# 计算准确度
+accuracy = accuracy_count / len(loan_status_result) * 100
+print("模型训练结果的准确度为: {:.2f}%".format(accuracy))
