4df68ff3f6fa67961d227e7d74720b8cc47848e9
li-haolun <130045057+Apocalypse20030529@users.noreply.github.com>
2024-05-15 22:06:08 +0800

    初步完成首页的静态页面设计

--- host/static/bitwallet/src/views/HomeView.vue
+++ host/static/bitwallet/src/views/HomeView.vue
@@ -16,24 +16,23 @@
 
           </el-header> -->
           <el-main class="main">
-            <el-row class="rows">
+            <div class="background-image">
+            </div>
+            <div class="background-color">
+              <el-col class="cols_top"></el-col>
               <el-col class="cols_left">我们的价值主张</el-col>
               <el-col class="cols_right">我们的独特技术</el-col>
-            </el-row>
-            <el-row class="rows">
               <el-col class="cols_left">数据隐私保护的高度可信性</el-col>
               <el-col class="cols_right">使用Tendermint搭建区块链</el-col>
-            </el-row>
-            <el-row class="rows">
               <el-col class="cols_left">去中心化的安全保障机制</el-col>
               <el-col class="cols_right">使用MPyC框架设计多方安全协议</el-col>
-            </el-row>
-            <el-row class="rows">
               <el-col class="cols_left">数据共享和模型融合的高效性</el-col>
               <el-col class="cols_right">使用PySyft框架用于搭建联邦学习</el-col>
-            </el-row>
+              <el-col class="cols_bottom"></el-col>
+            </div>
+            <img class="arrows_image" src="../images/arrows.jpg" alt="Element logo" />
           </el-main>
-          <el-footer class="Footer">Footer</el-footer>
+          <el-footer class="Footer"></el-footer>
         </el-container>
       </el-container>
     </el-container>
@@ -51,21 +50,55 @@ import LoginHead from "@/components/LoginHeader.vue";
 </script>
 
 <style>
-.rows{
+.background-image {
+  background-image: url('../images/main_bg.jpg'); /* 设置图片路径 */
+  background-size: cover; /* 背景图片覆盖整个元素 */
+  background-repeat: no-repeat; /* 背景图片不重复 */
+  background-position: center; /* 背景图片居中 */
+  width: 100%; /* 元素宽度 */
+  height: 84px; /* 元素高度 */
+}
+.background-color {
+  background-color:aqua;
+  background-size: cover; /* 背景图片覆盖整个元素 */
+  background-repeat: no-repeat; /* 背景图片不重复 */
+  background-position: center; /* 背景图片居中 */
+  width: 100%; /* 元素宽度 */
+  height: 84px; /* 元素高度 */
+}
+.cols_top{
   width:100%;
-  text-align: center;
-  display: block;
+  border-top: 1px solid black;
+  z-index:10;
+}
+.cols_bottom{
+  width:100%;
+  margin-top:82px;
+  border-bottom: 1px solid black;
+  z-index:10;
 }
 .cols_left{
-  width:50%;
+  width:48%;
   text-align: center;
   float:left;
   display: block;
+  font-size:16px;
+  border-left: 1px solid black;
+  color:white;
+  z-index:10;
 }
 .cols_right{
-  width:50%;
+  width:48%;
   text-align: center;
   float:right;
   display: block;
+  font-size:16px;
+  border-right: 1px solid black;
+  color:white;
+  z-index:10;
+}
+.arrows_image{
+  width: 100%; 
+  height: 75%;
 }
 </style>
